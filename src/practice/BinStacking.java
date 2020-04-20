a9cfdbcf6c3b2a37b4db99b02c8c66dc1d358ea6
Kevin <kelvinmacharia078@gmail.com>
2020-04-20 04:43:27 +0300

    KC-002: First fit bin packing algorithm

--- src/practice/BinStacking.java
+++ src/practice/BinStacking.java
@@ -6,6 +6,8 @@ public class BinStacking {
         int[] items = {2, 5, 4, 7, 1, 3, 8};
         System.out.println("Next Fit Bins Used: "+nextFit (items, 10));
 
+        System.out.println("First Fit Bins Used: "+firstFit (items, 10));
+
     }
 
     /**
@@ -26,6 +28,32 @@ public class BinStacking {
             } else binRem -= weight;
         }
 
+        return bins;
+    }
+
+    private static int firstFit(int[] weights, int capacity){
+        int bins = 0; // initialize bins used to zero
+        /* create an array to hold remaining capacity in bins,
+        * there can be at most n bins where n is the number of weights to be packed
+        * */
+        int[] rem_bin = new int[weights.length];
+
+        // let's now place each weight in the first bin where it fits
+        for (int weight : weights) {
+            // find the first bin that can accommodate the weight
+            int j;
+            for (j = 0; j < bins; j++) {
+                if (weight <= rem_bin[j]){
+                    rem_bin[j] -= weight;
+                    break;
+                }
+            }
+            // if no bin could accommodate the weight, use a new bin
+            if (j == bins) {
+                rem_bin[bins] = capacity - weight;
+                bins++;
+            }
+        }
         return bins;
     }
 }
