76d8f936ae8d78da9f12c9946eecce3b452c6978
Kevin <kelvinmacharia078@gmail.com>
2020-04-20 04:27:17 +0300

    KC-002: Next Fit bin packing algorithm

--- src/practice/BinStacking.java
+++ src/practice/BinStacking.java
@@ -3,13 +3,29 @@ package practice;
 
 public class BinStacking {
     public static void main(String[] args) {
+        int[] items = {2, 5, 4, 7, 1, 3, 8};
+        System.out.println("Next Fit Bins Used: "+nextFit (items, 10));
 
     }
 
-    private static int minimumBins(int[] items, int bins){
-        if (items.length == 0)
-            return 0;
+    /**
+     * This algorithm calculates the minimum number of bins required to pack the provided weights given the
+     * maximum bin capacity using the Next Fit algorithm
+     * @param weights the weights that are to be packed in the bins
+     * @param binCapacity the maximum bin capacity of each bean
+     * @return the minimum number of bins used
+     */
+    private static int nextFit(int[] weights, int binCapacity){
+        int binRem = binCapacity; // the remaining capacity of a bin
+        int bins = 0;
 
-        return 0;
+        for (int weight: weights) {
+            if (weight > binRem) {
+                bins++; // use another bin
+                binRem = binCapacity - weight;
+            } else binRem -= weight;
+        }
+
+        return bins;
     }
 }
