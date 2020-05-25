80ad84365042020d5f88e82cb86d980ed1c0da70
Kevin <kelvinmacharia078@gmail.com>
2020-05-25 18:42:57 +0300

    String compression function implementation

--- src/practice/StringOperations.java
+++ src/practice/StringOperations.java
@@ -2,9 +2,9 @@ package practice;
 
 public class StringOperations {
     public static void main(String[] args) {
-        String input = "";
+        String input = "aaabbccccddeee";
 
-        String output = compress(input);
+        String output = compressString(input);
         System.out.println(input +" => "+ output);
     }
 
@@ -15,10 +15,31 @@ public class StringOperations {
      * @param original the string that is being compressed
      * @return the compressed string if shorter, else the original
      */
-    private static String compress(String original){
+    private static String compressString(String original){
         if (original.length() > 2){
             StringBuilder compressed = new StringBuilder();
-            compressed.charAt(compressed.length());
+
+            // initialize the char counter to 0 and first compressed character to the first char on original string
+            compressed.append(original.charAt(0));
+            int count = 1;
+
+            for (int i = 1; i < original.length(); i++) {
+                if (compressed.charAt(compressed.length()-1) == original.charAt(i))
+                    count++;
+                else {
+                    compressed.append(count);
+                    compressed.append(original.charAt(i));
+                    count = 1;
+                }
+            }
+            // insert the count for the final character before you return
+            compressed.append(count);
+
+            // if the compressed string is longer than the original string, return the original
+            if (compressed.length() > original.length())
+                return original;
+
+            return compressed.toString();
         }
         return original;
     }
