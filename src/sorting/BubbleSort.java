17cbe33ab464ff448504aa57b7b587d6680fd0fb
Kevin Macharia <kelvinmacharia078@gmail.com>
2020-07-11 01:33:06 +0300

    documentation inclusion

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -2,6 +2,11 @@ package sorting;
 
 import java.util.Arrays;
 
+/**
+ * This class implements a Bubble sort algorithm
+ * Time complexity : O(n^2)
+ * Space complexity : O(1)
+ */
 public class BubbleSort {
     public static void main(String[] args) {
         int[] input = {12,4,7,3,5,1,9};
@@ -9,6 +14,15 @@ public class BubbleSort {
         System.out.println("Sorted: "+ Arrays.toString(solution(input)));
     }
 
+    /**
+     * Implementation of bubble sort algorithm.
+     * The largest element gets 'bubbled all the way to the end'
+     * and the process gets repeated for all elements in the input array
+     * The array is always sorted in place.
+     *
+     * @param array the input array which is to be sorted.
+     * @return a sorted version of the input array
+     */
     private static int[] solution(int[] array){
         for (int i = 0; i < array.length-1; i++) {
             int temp;
