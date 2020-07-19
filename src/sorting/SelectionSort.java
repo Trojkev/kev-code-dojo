0916e1ad3e159449c3fb495ecc8a47df4c36306e
Kevin Macharia <kelvinmacharia078@gmail.com>
2020-07-19 12:24:48 +0300

    Selection sort algorithm analysis and documentation

--- src/sorting/SelectionSort.java
+++ src/sorting/SelectionSort.java
@@ -9,6 +9,15 @@ public class SelectionSort {
                 "Unsorted: "+ Arrays.toString(input) +"\nSorted: "+ Arrays.toString(sortItems(input)));
     }
 
+    /**
+     * Selection sort algorithm implementation.
+     * We identify the smallest item in the input array and record it's index, then we swap the first unsorted
+     * element with the element at min index
+     * Time complexity : O(n^2) -> we are looping n times for every item in the array
+     * Space complexity : O(1) -> we are only using two variables and sorting array in space
+     * @param array the input array to be sorted
+     * @return a sorted version of the input array
+     */
     private static int[] sortItems(int[] array){
         for (int i = 0; i < array.length; i++) {
             int minIndex = i;
