5ba684fce492a79fca6dfaf2beb50a9974bc9776
Kevin Macharia <KMACHARIA@safaricom.co.ke>
2020-10-19 13:17:14 +0300

    Maximum sub array sum for a specified size in an array

--- src/dynamic_programming/SlidingWindow.java
+++ src/dynamic_programming/SlidingWindow.java
@@ -4,6 +4,10 @@ public class SlidingWindow {
     public static void main(String[] args) {
         String text = "aabaaabbabbaabbb";
         System.out.println("Longest Substring: "+ findLongestSubstring(text));
+
+        int[] numbers = {2,5,3,8,7,1,4,1};
+        int size = 3;
+        System.out.println("Max sub-array sum: "+ maxSubArraySum(numbers, size));
     }
 
     /**
@@ -32,11 +36,32 @@ public class SlidingWindow {
                 i--;
             } else runningCount ++;
 
-            System.out.printf(
-                    "Index: %d, Char: %s, charCount: %d, runningCount: %d, maxLen: %d%n",
-                    i, text.charAt(i), charCount, runningCount, maxLen);
+//            System.out.printf(
+//                    "Index: %d, Char: %s, charCount: %d, runningCount: %d, maxLen: %d%n",
+//                    i, text.charAt(i), charCount, runningCount, maxLen);
         }
 
         return maxLen;
+    }
+
+    /**
+     * This function calculates the largest sum obtained by summing a contiguous sub array of size k
+     * @param array an array of integers to be processed
+     * @param k the size of the sub array to be computed
+     * @return the largest sum possible
+     */
+    private static int maxSubArraySum(int[] array, int k){
+        int maxSum = Integer.MIN_VALUE;
+        int runningCount = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            runningCount += array[i];
+            if (i >= k-1){
+                maxSum = Math.max(maxSum, runningCount);
+                runningCount -= array[i - (k-1)];
+            }
+        }
+
+        return maxSum;
     }
 }
