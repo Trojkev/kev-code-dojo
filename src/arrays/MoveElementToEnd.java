94d2136b8f4b0e623ccc24a73d538c05e34de65c
Kevin Macharia <kelvinmacharia078@gmail.com>
2020-11-17 16:49:46 +0300

    Move element to the end of array optimal solution

--- src/arrays/MoveElementToEnd.java
+++ src/arrays/MoveElementToEnd.java
@@ -18,7 +18,8 @@ public class MoveElementToEnd {
 
         int toMove = 2;
         System.out.println("Input: "+nums);
-        System.out.println("Output: "+moveElementToEnd(nums, toMove));
+        System.out.println("First Output: "+moveElementToEnd(nums, toMove));
+        System.out.println("Optimal Output: "+moveElementToEndOptimal(nums, toMove));
     }
 
     private static List<Integer> moveElementToEnd(List<Integer> array, int element){
@@ -33,6 +34,28 @@ public class MoveElementToEnd {
 
         for (int i = 0; i < counter; i++) {
             array.add(element);
+        }
+
+        return array;
+    }
+
+    private static List<Integer> moveElementToEndOptimal(List<Integer> array, int element) {
+        int firstIdx = 0;
+        int lastIdx = array.size() - 1;
+
+        // iterate as long as the two pointers don't overlap
+        while (lastIdx > firstIdx){
+            // move the last pointer backward until we find a candidate for swapping
+            while (array.get(lastIdx) == element)
+                lastIdx--;
+
+            // swap the elements at the pointers if they are valid candidates
+            if (array.get(firstIdx) == element){
+                int temp = array.get(firstIdx);
+                array.set(firstIdx, array.get(lastIdx));
+                array.set(lastIdx, temp);
+            }
+            firstIdx++;
         }
 
         return array;
