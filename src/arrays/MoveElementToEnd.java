941dcf49700c965e97609f5b3b3f177f2ad46282
Kevin Macharia <KMACHARIA@safaricom.co.ke>
2020-11-17 15:43:49 +0300

    Moving elements to the end on an array try one

---
+++ src/arrays/MoveElementToEnd.java
@@ -0,0 +1,41 @@
+package arrays;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class MoveElementToEnd {
+    public static void main(String[] args) {
+        List<Integer> nums = new ArrayList<>();
+        nums.add(2);
+        nums.add(4);
+        nums.add(1);
+        nums.add(2);
+        nums.add(5);
+        nums.add(6);
+        nums.add(3);
+        nums.add(2);
+
+        int toMove = 2;
+        System.out.println("Input: "+nums);
+        System.out.println("Output: "+moveElementToEnd(nums, toMove));
+    }
+
+    private static List<Integer> moveElementToEnd(List<Integer> array, int element){
+        int counter = 0;
+        Iterator<Integer> itr = array.iterator();
+        while (itr.hasNext()){
+            int val = itr.next();
+            if (val == element){
+                counter++;
+                itr.remove();
+            }
+        }
+
+        for (int i = 0; i < counter; i++) {
+            array.add(element);
+        }
+
+        return array;
+    }
+}
