a20c2fc23847e85ec249e946ae094f1683f66ecd
Kevin Macharia <kelvinmacharia078@gmail.com>
2021-03-15 15:54:03 +0300

    Find second largest node in BST tests

--- src/trees/SecondLargestNodeInBST.java
+++ src/trees/SecondLargestNodeInBST.java
@@ -13,6 +13,14 @@ public class SecondLargestNodeInBST {
     }
     public static void main(String[] args) {
         BST root = new BST(10);
+        root.right = new BST(18);
+        root.left = new BST(6);
+
+        root.right.right = new BST(25);
+        root.right.left = new BST(15);
+
+        root.left.right = new BST(8);
+        root.left.left = new BST(3);
 
         System.out.println("Second largest node is: " + findSecondLargestNode(root));
     }
@@ -25,7 +33,7 @@ public class SecondLargestNodeInBST {
 
         while(currentNode != null) {
             if(currentNode.right == null) break;
-            
+
             parent = currentNode;
             currentNode = currentNode.right;
         }
