99be83d12f0c71029419b712faa0a1d7520bd204
Kevin Macharia <kelvinmacharia078@gmail.com>
2021-03-15 15:40:50 +0300

    List files in a directory recursively

---
+++ src/recursion/FilesInDirectory.java
@@ -0,0 +1,24 @@
+package recursion;
+
+import java.io.File;
+
+public class FilesInDirectory {
+    public static void main(String[] args) {
+        String pathName = "D:/projects/kev-code-dojo";
+        listFilesInDirectory(pathName);
+    }
+
+    private static void listFilesInDirectory(String path) {
+        File file = new File(path);
+
+        String[] fileNames = file.list();
+
+        for (String fName : fileNames) {
+            if(new File(path +"/"+fName).isDirectory()){
+				listFilesInDirectory(path +"/"+fName);
+			} else {
+				System.out.println(path +"/"+fName);
+			}
+        }
+    }
+}
