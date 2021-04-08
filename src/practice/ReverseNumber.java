494faadc2d09a0abcb46aa1a066ab16edc308e2a
Kevin Macharia <kelvinmacharia078@gmail.com>
2021-04-08 08:31:29 +0300

    Reverse number optimised

--- src/practice/ReverseNumber.java
+++ src/practice/ReverseNumber.java
@@ -8,16 +8,9 @@ public class ReverseNumber {
     }
 
     private static int reverseNumber(int N) {
-        int enable_print = N % 10;
         int num=0;
         while (N > 0) {
             num = num * 10 + N % 10;
-            if (enable_print == 0 && N % 10 != 0) {
-                enable_print = 1;
-            }
-            else if (enable_print == 1) {
-                System.out.print(N % 10);
-            }
             N = N / 10;
         }
         return num;
